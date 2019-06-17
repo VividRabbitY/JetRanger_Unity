4b826930323a222f1c0457710ca662bbc14ab77a
vivid_rabbit <sakurames84@gmail.com>
2019-06-17 10:07:25 +0900

    Add Unity Project

---
+++ Assets/BulletMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public float bulletSpeed=0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.right * bulletSpeed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    private bool canShoot;
+    private float shootCooldown=0.1f;
+    private float timer;
+
+    public GameObject Bullet;
+    public Transform shootPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canShoot = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(Input.GetButton("Fire2")&&canShoot)
+        {
+            Instantiate(Bullet, shootPoint.position, Quaternion.identity);
+            canShoot = false;
+            timer = 0f;
+        }
+        if(timer>shootCooldown)
+        {
+            canShoot = true;
+        }
+
+    }
+}

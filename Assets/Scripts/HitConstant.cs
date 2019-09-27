ade81d396541bae85d3f3e9b6f6006ae678696d3
VividRabbitY <45251813+VividRabbitY@users.noreply.github.com>
2019-09-27 10:40:30 +0900

    Update HitConstant.cs

--- Assets/Scripts/HitConstant.cs
+++ Assets/Scripts/HitConstant.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//当たり判定関連半径
 public static class HitConstant
 {
+    //プレイヤー
     public static float playerHitRadio = 0.25f;
+    //エネミー
     public static float enemyBaseHitRaidio = 0.8f;
-
+    //プレイヤー弾
     public static float playerShotRadio = 0.4f+enemyBaseHitRaidio;
+    //エネミー弾
     public static Dictionary<EnemyshotType, float> enemyShotHitRadio = new Dictionary<EnemyshotType, float>()
     {
         { EnemyshotType.SmallBall,0.2f+playerHitRadio },

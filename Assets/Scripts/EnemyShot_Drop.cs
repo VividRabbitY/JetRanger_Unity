fa8727e75c293d048aafda682142948d4ac847fe
VividRabbitY <45251813+VividRabbitY@users.noreply.github.com>
2019-09-27 10:35:36 +0900

    Update EnemyShot_Drop.cs

--- Assets/Scripts/EnemyShot_Drop.cs
+++ Assets/Scripts/EnemyShot_Drop.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//上空から落ちる弾
+//画面後ろから前の順で波状落ちる
 public class EnemyShot_Drop : MonoBehaviour
 {
     Vector3 velocity;
@@ -9,14 +11,14 @@ public class EnemyShot_Drop : MonoBehaviour
     float timer;
 
     float prepareTime;
-    public float maxPrepareTime; 
+    public float maxPrepareTime; //一番後ろ落ちる時間
     // Start is called before the first frame update
     void Start()
     {
         velocity = Vector3.down;
         speed = 0.0f;
         timer = 0.0f;
-
+        //生成から落ちるまでの時間計算
         prepareTime = 1.0f + (1-((transform.position.x - FieldSetting.fieldMin.x) / 18.0f)) * maxPrepareTime;
 
     }
